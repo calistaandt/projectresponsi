3f09f5f48633c7da2f7e8569d781e49c8f48de90
calistaandt <calistaandta@gmail.com>
2023-11-21 15:36:26 +0700

    commit kedua

--- registrasi.php
+++ registrasi.php
@@ -8,31 +8,18 @@ if(isset($_POST["registrasi"])){
     $password = $_POST['password'];
     $password2= $_POST['password2'];
 
-    $sql= mysqli_num_rows(mysqli_query($conn, "select * from pengguna where username= '$username' "));
+    $sql= mysqli_num_rows(mysqli_query($conn, "SELECT * FROM pengguna WHERE username= '$username' "));
     if($sql > 0){
-        ?>
-        <script>
-            alert("Username Anda Telah Digunakan! Silakan Ganti Username Anda.")
-        </script>
-        <?php
+        echo "Username Anda Telah Digunakan! Silakan Ganti Username Anda.";
     }else{
         if($password == $password2){
-            $insert = "insert into pengguna(id, nama, username, password) values ('$id', '$nama','$username', '$password')";
+            $insert = "INSERT INTO pengguna(id, nama, username, password) VALUES ('$id', '$nama','$username', '$password')";
             $query = mysqli_query($conn, $insert);
             if($query){
-                ?>
-                <script>
-                    alert("Registrasi Berhasil!"); 
-                    document.location='login.php';
-                </script>
-                <?php
+                header("Location: login.php");
             }
         }else{
-            ?>
-            <script>
-                alert("Periksa Data Kembali!");
-            </script>
-            <?php
+            echo "Periksa Data Kembali!";
         }
     }
     
@@ -50,23 +37,23 @@ if(isset($_POST["registrasi"])){
 <body>
     <center>
     <h1>REGISTRASI</h1>
-    <form name="registrasi" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data" onsubmit="return cek()">
+    <form name="registrasi" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
         <table>
             <tr>
                 <td>
-                    <input type="text" name="nama" id="fieldnama" placeholder="Nama"></td>
+                    <input type="text" name="nama" placeholder="Nama"></td>
             </tr>
             <tr>
                 <td>
-                    <input type="text" name="username" id="fielduser" placeholder="Username"></td>
+                    <input type="text" name="username" placeholder="Username"></td>
             </tr>
             <tr>
                 <td>
-                    <input type="password" name="password" id="fieldpass1" placeholder="Password"></td>
+                    <input type="password" name="password" placeholder="Password"></td>
             </tr>
             <tr>
                 <td>
-                    <input type="password" name="password2" id="fieldpass2" placeholder="Konfirmasi Password"></td>
+                    <input type="password" name="password2" placeholder="Konfirmasi Password"></td>
             </tr>
             <tr>
                 <td><input type="submit" name="registrasi" value="Registrasi"></td>
@@ -75,36 +62,5 @@ if(isset($_POST["registrasi"])){
     </form>
     <p>Sudah memiliki akun? <a href="login.php">Login</a></p>
     </center>
-    <script>
-        function cek() {
-            const fieldnama = document.getElementById("fieldnama");
-            if (fieldnama.value == "") {
-                alert("Nama Harus Diisi!");
-                fieldnama.focus();
-                return false;
-            }
-
-            const fielduser = document.getElementById("fielduser");
-            if (fielduser.value == "") {
-                alert("Username Harus Diisi!");
-                fielduser.focus();
-                return false;
-            }
-
-            const fieldpass1 = document.getElementById("fieldpass1");
-            if (fieldpass1.value == "") {
-                alert("Password Harus Diisi!");
-                fieldpass1.focus();
-                return false;
-            }
-
-            const fieldpass2 = document.getElementById("fieldpass2");
-            if (fieldpass2.value == "") {
-                alert("Konfirmasi Password Harus Diisi!");
-                fieldpass2.focus();
-                return false;
-            }
-        }
-    </script>
 </body>
 </html>
